f7f4daa657acc89867963f8890e10b31f273e8d4
viwnu <xenmysafitiplace@gmail.com>
2025-03-11 09:42:47 +0300

    add spreadsheets seed data

--- src/postgres/seeds/spreadsheets.js
+++ src/postgres/seeds/spreadsheets.js
@@ -4,7 +4,13 @@
  */
 export async function seed(knex) {
     await knex("spreadsheets")
-        .insert([{ spreadsheet_id: "some_spreadsheet" }])
+        .insert([
+            { spreadsheet_id: "some_spreadsheet" },
+            { spreadsheet_id: "1FhCqAKpPgFMjTAKHvdvLKBpqJWh8ugQPPcwJVKy7xlU" },
+            { spreadsheet_id: "1jihbGMDKoPjm8Ch44QuZoSKPWEbizUP_59-Id4PEHAQ" },
+            { spreadsheet_id: "1AKi2ROHj9hXB2HIqWn85buN8bwu5bHtPzOcHo33YFbs" },
+            { spreadsheet_id: "1g1pT60rnGvwKrhYa1SAK7_Tz5qFxZJyDkhFSyXs-ANA" },
+        ])
         .onConflict(["spreadsheet_id"])
         .ignore();
 }
